061ece1eb4bfb187dfcffc9add50f4f0d7bc66c0
Pietroo03 <pietro.ponte.03@gmail.com>
2025-03-17 18:38:15 +0100

    add pages for single song and film add bootstrap add style

--- src/main/java/org/exercise/java/spring/best_of_the_year/controllers/HomeController.java
+++ src/main/java/org/exercise/java/spring/best_of_the_year/controllers/HomeController.java
@@ -8,18 +8,13 @@ import org.exercise.java.spring.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-
-    private final BestOfTheYearApplication bestOfTheYearApplication;
-
-    HomeController(BestOfTheYearApplication bestOfTheYearApplication) {
-        this.bestOfTheYearApplication = bestOfTheYearApplication;
-    }
 
     @GetMapping("/")
     public String home(@RequestParam(name = "name") String name, Model model) {
@@ -43,19 +38,58 @@ public class HomeController {
     }
 
     @GetMapping("/movies")
-    public String Movies(Model model) {
+    public String movies(Model model) {
         List<Movie> movies = getBestMovies();
 
         model.addAttribute("movies", movies);
+        model.addAttribute("currentPage", "/movies");
         return "movies";
     }
 
     @GetMapping("/songs")
-    public String Songs(Model model) {
+    public String songs(Model model) {
         List<Song> songs = getBestSongs();
 
         model.addAttribute("songs", songs);
+        model.addAttribute("currentPage", "/songs");
+
         return "songs";
+    }
+
+    @GetMapping("/movies/{id}")
+    public String movieById(Model model, @PathVariable("id") int id) {
+
+        List<Movie> movies = getBestMovies();
+        Movie foundMovie = null;
+
+        for (Movie movie : movies) {
+            if (movie.getId() == id) {
+                foundMovie = movie;
+                break;
+            }
+        }
+
+        model.addAttribute("movie", foundMovie);
+        model.addAttribute("id", id);
+        return "singleMovie";
+    }
+
+    @GetMapping("/songs/{id}")
+    public String songById(Model model, @PathVariable("id") int id) {
+
+        List<Song> songs = getBestSongs();
+        Song foundSong = null;
+
+        for (Song song : songs) {
+            if (song.getId() == id) {
+                foundSong = song;
+                break;
+            }
+        }
+
+        model.addAttribute("song", foundSong);
+        model.addAttribute("id", id);
+        return "singleSong";
     }
 
 }
